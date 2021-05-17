0068fe39b45f33fdb90397ab06cd8ae1baaa695a
Lior Kashti <lkashti@gmail.com>
2021-05-17 19:30:29 +0300

    installed cors, use axios for requests

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const PORT = 3000;
 
 mongoose.connect(process.env.DATABASE_URL, {
@@ -13,11 +14,12 @@ const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to database"));
 
+// app.use(cors());
 app.use(express.json());
-
+app.use(cors())
 
 const locationsRouter = require("./routes/locations");
-app.use("/locations",locationsRouter);
+app.use("/locations", locationsRouter);
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
